e82bba3be3dc3d4b7b31cbbecc19a05a2af8c27d
Sherine Coronel <sherinecoronel@gmail.com>
2016-12-15 23:37:11 +0800

    Deleted unnecessary php file and still fixing the others

--- Exercise6/codeigniter/application/views/Contact.php
+++ Exercise6/codeigniter/application/views/Contact.php
@@ -9,6 +9,24 @@
 	div{
 		text-align:center;
 	}
+	table, th, td {
+		text-align:center;
+		border: 1px dashed gray;
+		border-collapse: collapse;
+	}
+	table#t01 th {
+		background-color: white;
+		color: black;
+	}
+	table#t01 td {
+		background-color: white;
+		color: black;
+		font-size:5
+	}
+	th, td {
+		padding: 10px;
+		text-align: center;
+	}
 	a:link {
 		color: black;
 		width: 100%;
@@ -20,12 +38,64 @@
 	a:hover {
 		color: hotpink;
 	}
+		body{
+		font-family: Georgia;
+	}
+	div{
+		text-align:center;
+	}
+	table, th, td {
+		text-align:center;
+		border: 1px dashed gray;
+		border-collapse: collapse;
+	}
+	table#t01 th {
+		background-color: white;
+		color: black;
+	}
+	table#t01 td {
+		background-color: white;
+		color: black;
+		font-size:5
+		
+	}
+	th, td {
+		padding: 10px;
+		text-align: center;
+	}
+	a:link {
+		color: black;
+		width: 100%;
+		text-decoration: none;
+	}
+	a:visited {
+    color: gray;
+	}
+	a:hover {
+	color: hotpink;
+	}
+	.button {
+    background-color: rgba(195, 195, 195, 0.3);
+    border: none;
+    color: gray;
+    padding: 6px 12px;
+    text-align: center;
+    text-decoration: none;
+    font-size: 15px;
+    margin: 4px 2px;
+    cursor: pointer;
+    transition-duration: 0.3s;
+	}
+	.button2:hover {
+	color: hotpink;
+	
+	}
 </style>
-<div style="text-align:center; margin-top:3em; margin-bottom:1em">
+<div style="text-align:center; margin-top:3em; margin-bottom:5em">
 	<img src="name.jpg" style="width:721px;height:80px;">
 </div>
 
-<div id="link" style = "margin-top:5em">
+<div id="link" style = "margin-top:1em">
 	<p>
 	<a href="Coronel.html">HOME</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 	<a href="Gallery.html">GALLERY</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
@@ -34,19 +104,61 @@
 	<a href="Contact.html">CONTACT</a>
 	</p>
 </div>
+<center>
+<div  style="margin-top:3em; margin-bottom:3em">
+<p>My date of birth:</p>
+<a class="button button2" id="demo" onclick="oneFunction()">Click me for answer!</a>
 
-<p style = "margin-left:15em;margin-top:5em">
-EMAIL ME AT <br>
-</p>
-<p style = "margin-left:20em;margin-top:1em">
-sherinecoronel@gmail.com
-</p>
-<p style = "margin-left:20em;margin-top:1em">
-shen_plim@yahoo.com
-</p>
+<script>
+function oneFunction() {
+    document.getElementById("demo").innerHTML = "June 04, 1999";
+}
+</script>
 
-<div  style="margin-top:4em; margin-bottom:4em">
-</div>	
+</div>
+<div  style="margin-top:3em; margin-bottom:3em">
+<p>My favorite music genre:</p>
+<a class="button button2" id="demo2" onclick="twoFunction()">Click me for answer!</a>
+
+<script>
+function twoFunction() {
+    document.getElementById("demo2").innerHTML = "Rhythm and Blues";
+}
+</script>
+</div>
+
+<div  style="margin-top:3em; margin-bottom:3em">
+<p>Who I love the most?</p>
+<a class="button button2" id="demo3" onclick="threeFunction()">Click me for answer!</a>
+
+<script>
+function threeFunction() {
+    document.getElementById("demo3").innerHTML = "My mom";
+}
+</script>
+</div>
+
+<div  style="margin-top:3em; margin-bottom:3em">
+<p>My zodiac sign:</p>
+<a class="button button2" id="demo4" onclick="fourFunction()">Click me for answer!</a>
+
+<script>
+function fourFunction() {
+    document.getElementById("demo4").innerHTML = "I'm a typical Gemini";
+}
+</script>
+</div>
+
+<div  style="margin-bottom:3em">
+<p>What frightens me?</p>
+<a class="button button2" id="demo5" onclick="fiveFunction()">Click me for answer!</a>
+
+<script>
+function fiveFunction() {
+    document.getElementById("demo5").innerHTML = "Death";
+}
+</script>
+</div>
 <center>
 <hr	size="2px" width="75%" color="gray">
 <div  style="margin-top:3em">
@@ -72,3 +184,4 @@ shen_plim@yahoo.com
 </div>
 </body>
 </html>
+
