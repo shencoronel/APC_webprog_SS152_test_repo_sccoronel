2c09c7fb9803136b299b88edb67d069d97cb0a08
shencoronel <sherinecoronel@gmail.com>
2016-12-16 20:20:50 +0800

    Gallery is now fixed

--- Exercise6/codeigniter/application/views/Gallery.php
+++ Exercise6/codeigniter/application/views/Gallery.php
@@ -22,7 +22,7 @@
 	}
 </style>
 <div style="text-align:center; margin-top:3em; margin-bottom:1em">
-	<img src="name.jpg" style="width:721px;height:80px;">
+	<img src="<?php echo base_url();?>/images/name.jpg" style="width:721px;height:80px;">
 </div>
 
 <div id="link" style = "margin-top:5em">
@@ -36,17 +36,17 @@
 </div>
 
 <div style="margin-top:4em">
-<img src="gal1.jpg" width="300" height="300"/>
-<img src="gal2.jpg" width="300" height="300"/>
-<img src="gal3.jpg" width="300" height="300"/>
+<img src="<?php echo base_url();?>/images/gal1.jpg" width="300" height="300"/>
+<img src="<?php echo base_url();?>/images/gal2.jpg" width="300" height="300"/>
+<img src="<?php echo base_url();?>/images/gal3.jpg" width="300" height="300"/>
 <br>
-<img src="gal4.jpg" width="300" height="300"/>
-<img src="gal5.jpg" width="300" height="300"/>
-<img src="gal6.jpg" width="300" height="300"/>
+<img src="<?php echo base_url();?>/images/gal4.jpg" width="300" height="300"/>
+<img src="<?php echo base_url();?>/images/gal5.jpg" width="300" height="300"/>
+<img src="<?php echo base_url();?>/images/gal6.jpg" width="300" height="300"/>
 <br>
-<img src="gal7.jpg" width="300" height="300"/>
-<img src="gal8.jpg" width="300" height="300"/>
-<img src="gal9.jpg" width="300" height="300"/>
+<img src="<?php echo base_url();?>/images/gal7.jpg" width="300" height="300"/>
+<img src="<?php echo base_url();?>/images/gal8.jpg" width="300" height="300"/>
+<img src="<?php echo base_url();?>/images/gal9.jpg" width="300" height="300"/>
 
 </div>
 <div style = "margin-top:5em">
@@ -61,19 +61,19 @@
 </div>
 <div  style="margin-top:1em; margin-bottom:3em">
 <a href="https://www.facebook.com/sherinecoronel" target="_blank" style="text-decoration:none">
-<img src="fb1.jpg "
+<img src="<?php echo base_url();?>/images/fb1.jpg "
   alt="Facebook"
   width="50"height="50"onmouseover="this.src='ic2.png'" onmouseout="this.src='fb1.jpg'"/> </a>
 <a href="https://www.twitter.com/shencoronel" target="_blank" style="text-decoration:none">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-<img src="twitter1.jpg"
+<img src="<?php echo base_url();?>/images/twitter1.jpg"
   alt="Twitter"
   width="50"height="50"onmouseover="this.src='ic.png'" onmouseout="this.src='twitter1.jpg'"/> </a>
 <a href="https://www.snapchat.com/add/shencoronel" target="_blank" style="text-decoration:none">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-<img src="snap1.jpg"
+<img src="<?php echo base_url();?>/images/snap1.jpg"
   alt="Snapchat"
   width="50"height="50"onmouseover="this.src='ic3.png'" onmouseout="this.src='snap1.jpg'"/> </a>
 <a href="http://www.malawaknautak.tumblr.com" target="_blank" style="text-decoration:none">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-<img src="tumblr1.jpg"
+<img src="<?php echo base_url();?>/images/tumblr1.jpg"
   alt="Tumblr"
   width="50"height="50"onmouseover="this.src='ic4.png'" onmouseout="this.src='tumblr1.jpg'"/> </a>
 </div>
