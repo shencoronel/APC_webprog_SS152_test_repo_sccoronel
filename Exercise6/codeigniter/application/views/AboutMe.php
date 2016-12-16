329ef454fb01913dce299addcbc67566dd5304c9
shencoronel <sherinecoronel@gmail.com>
2016-12-16 20:18:04 +0800

    Fixed photos for AboutMe site

--- Exercise6/codeigniter/application/views/AboutMe.php
+++ Exercise6/codeigniter/application/views/AboutMe.php
@@ -92,7 +92,7 @@
 	}
 </style>
 <div style="text-align:center; margin-top:3em; margin-bottom:5em">
-	<img src="name.jpg" style="width:721px;height:80px;">
+	<img src="<?php echo base_url();?>/images/name.jpg" style="width:721px;height:80px;">
 </div>
 
 <div id="link" style = "margin-top:1em">
@@ -166,19 +166,19 @@ function fiveFunction() {
 </div>
 <div  style="margin-top:1em; margin-bottom:3em">
 <a href="https://www.facebook.com/sherinecoronel" target="_blank" style="text-decoration:none">
-<img src="fb1.jpg "
+<img src="<?php echo base_url();?>/images/fb1.jpg "
   alt="Facebook"
   width="50"height="50"onmouseover="this.src='ic2.png'" onmouseout="this.src='fb1.jpg'"/> </a>
 <a href="https://www.twitter.com/shencoronel" target="_blank" style="text-decoration:none">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-<img src="twitter1.jpg"
+<img src="<?php echo base_url();?>/images/twitter1.jpg"
   alt="Twitter"
   width="50"height="50"onmouseover="this.src='ic.png'" onmouseout="this.src='twitter1.jpg'"/> </a>
 <a href="https://www.snapchat.com/add/shencoronel" target="_blank" style="text-decoration:none">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-<img src="snap1.jpg"
+<img src="<?php echo base_url();?>/images/snap1.jpg"
   alt="Snapchat"
   width="50"height="50"onmouseover="this.src='ic3.png'" onmouseout="this.src='snap1.jpg'"/> </a>
 <a href="http://www.malawaknautak.tumblr.com" target="_blank" style="text-decoration:none">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-<img src="tumblr1.jpg"
+<img src="<?php echo base_url();?>/images/tumblr1.jpg"
   alt="Tumblr"
   width="50"height="50"onmouseover="this.src='ic4.png'" onmouseout="this.src='tumblr1.jpg'"/> </a>
 </div>
